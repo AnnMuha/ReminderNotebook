c245f47ce297771a6a5792c76893bd1c1bd516fb
Муха Анна Вікторівна <vt231_mav@student.ztu.edu.ua>
2025-05-20 15:30:52 +0300

    feat: create MainWindow UI with binding to MainViewModel

---
+++ ReminderNotebook/Utils/PriorityColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using ReminderNotebook.Models;
+
+namespace ReminderNotebook.Utils
+{
+    public class PriorityColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ReminderPriority priority)
+            {
+                return priority switch
+                {
+                    ReminderPriority.Low => Brushes.Green,
+                    ReminderPriority.Medium => Brushes.Orange,
+                    ReminderPriority.High => Brushes.Red,
+                    _ => Brushes.Gray
+                };
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
