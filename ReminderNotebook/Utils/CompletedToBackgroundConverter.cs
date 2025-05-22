27275b160d76990a3c14caffa8ca9223b0789669
Муха Анна Вікторівна <vt231_mav@student.ztu.edu.ua>
2025-05-22 23:35:38 +0300

    feat: added completion checkbox and visual highlight for completed reminders

---
+++ ReminderNotebook/Utils/CompletedToBackgroundConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ReminderNotebook.Utils
+{
+    public class CompletedToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isCompleted && isCompleted)
+            {
+                return new SolidColorBrush(Color.FromRgb(200, 255, 200)); // світло-зелений
+            }
+
+            return new SolidColorBrush(Color.FromRgb(236, 240, 241)); // стандартний (як було)
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
